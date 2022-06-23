f92edbdc7234c89ee3218957f095140c00b3b8c2
Weredime <100447465+Weredime@users.noreply.github.com>
2022-06-23 17:12:03 +0100

    dont use 'var'; use 'const' and 'let'

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
-const fetch = require('node-fetch')
-const crypto = require('crypto')
+const crypto = require('crypto');
 
+const cors = require('cors');
+const fetch = require('node-fetch');
 const express = require('express');
-const db = require('monk')(process.env.MONGO_URL || 'localhost/my-ocular-other')
+const monk = require('monk')
 
+const db = monk(process.env.MONGO_URL || 'localhost/my-ocular-other')
 const users = db.get('users')
 const stars = db.get('stars')
 const reactions = db.get('reactions')
@@ -14,9 +16,6 @@ users.createIndex('name', { unique: true })
 
 const app = express()
 const port = 8081
-
-let cors = require('cors')
-
 
 const jokes = require('./jokes.json')
 const frontendURL = process.env.FRONTEND_URL || 'http://localhost:8000'
@@ -39,30 +38,30 @@ app.options('/api/users', cors(corsOptions)) // enable pre-flight request for us
 
 app.get('/api/users', cors(corsOptions), async (req, res) => {
     // const page = parseInt(req.query.page) || 0;
-    // let userList = await users.find({}, { sort: { _id: -1 }, limit: 15, skip: page * 15 })
-    if (!req.headers.authorization) {
-        res.json({ error: 'you need auth' })
-    } else {
-        let session = findSession(req.headers.authorization)
+    // const userList = await users.find({}, { sort: { _id: -1 }, limit: 15, skip: page * 15 })
+    if (typeof req.headers.authorization === "string") {
+        const session = findSession(req.headers.authorization)
 
-        if (!session) {
+        if (typeof session === "undefined") {
             return res.json({ error: 'invalid auth' })
         }
 
-        let sessionUser = await getUserData(session.name)
+        const sessionUser = await getUserData(session.name)
 
         if (!sessionUser.admin) {
             return res.json({ error: `only admins can get a list of users.` })
         }
-        let userList = await users.find({}, { sort: { "meta.updated": -1, _id: -1 } }) // TODO: pagination (see above)
-        res.json(userList)
+        const userList = await users.find({}, { sort: { "meta.updated": -1, _id: -1 } }) // TODO: pagination (see above)
+        return res.json(userList)
     }
+
+    res.json({ error: 'you need auth' })
 })
 
 app.get('/api/user/:name', cors(), async (req, res) => {
-    let noReplace = req.query.noReplace
-    let user = await getUserData(req.params.name.replace('*', ''))
-    let allUsers = await users.find()
+    const noReplace = typeof req.query.noReplace === "string";
+    const user = await getUserData(req.params.name.replace('*', ''))
+    const allUsers = await users.find()
 
     if (!noReplace && user) {
         user.status = user.status.replace(/(?<!\\){joke}/g, jokes[Math.floor(Math.random() * jokes.length)])
@@ -75,14 +74,22 @@ app.get('/api/user/:name', cors(), async (req, res) => {
         user.status = user.status.replace(/\\({total})/g, "$1")
 
         if (user.status.match(/(?<!\\){count}/)) {
-            let apiRes = await fetch(`https://scratchdb.lefty.one/v3/forum/user/info/${user.name}`)
-            var count = 0;
-            if(!apiRes.ok) {
-                count = "error"
-            } else {
-                let data = await apiRes.json()
-                count = data.counts.total.count
-            }
+            let count = "error"; // dont use number since it errors
+            try {
+                const controller = new AbortController();
+                const { signal } = controller;
+                setTimeout(() => {
+                    controller.abort()
+                }, 5000) // abort fetch after 5 seconds
+                const apiRes = await fetch(`https://scratchdb.lefty.one/v3/forum/user/info/${user.name}`, {
+                    signal: signal
+                });
+
+                if (apiRes.ok) {
+                    const data = await apiRes.json();
+                    count = data?.counts?.total?.count?.toString() || "error";
+                }
+            } catch {};
 
             user.status = user.status.replace(/(?<!\\){count}/g, count)
             user.status = user.status.replace(/\\({count})/g, "$1")
@@ -95,22 +102,20 @@ app.get('/api/user/:name', cors(), async (req, res) => {
 app.options('/api/user/:name', cors(corsOptions)) // enable pre-flight request for updating user
 
 app.put('/api/user/:name', cors(), async (req, res) => {
-    if (!req.headers.authorization) {
-        res.json({ error: 'you need auth' })
-    } else {
-        let session = findSession(req.headers.authorization)
+    if (typeof req.headers.authorization === "string") {
+        const session = findSession(req.headers.authorization)
 
-        if (!session) {
+        if (typeof session === "undefined") {
             return res.json({ error: 'invalid auth' })
         }
 
-        let sessionUser = await getUserData(session.name)
+        const sessionUser = await getUserData(session.name)
 
         if (session.name.toLowerCase() !== req.params.name.toLowerCase() && !sessionUser.admin) {
             return res.json({ error: `editing someone else's status i see.` })
         }
 
-        let user = await getUserData(req.params.name)
+        const user = await getUserData(req.params.name)
 
         // temporary security fix
         // return res.json({ error: 'for security reasons ocular statuses can not be updated at this time. sorry for the inconvenience' })
@@ -118,7 +123,7 @@ app.put('/api/user/:name', cors(), async (req, res) => {
         if (user) {
             if (user.banned && !sessionUser.admin) return res.json({ error: `you are banned from ocular. visit https://my-ocular.jeffalo.net/ban-info/${user.name} for more information.` })
 
-            let now = new Date()
+            const now = new Date()
 
             if (sessionUser.admin) {
                 // ban user
@@ -132,18 +137,18 @@ app.put('/api/user/:name', cors(), async (req, res) => {
             await users.update({ name: user.name }, { $set: { status: req.body.status, color: req.body.color, "meta.updatedBy": sessionUser.name, "meta.updated": now.toISOString() } })
 
 
-            res.json({ ok: 'user updated' })
+            return res.json({ ok: 'user updated' })
         } else {
             // this is an admin trying to update the status of a non-existent user. we should create that user with the specified data.
 
-            let scratchResponse = await fetch(`https://api.scratch.mit.edu/users/${req.params.name}/`) // get the proper case of the username instead of whatever admin inputted
-            let scratchData = await scratchResponse.json()
+            const scratchResponse = await fetch(`https://api.scratch.mit.edu/users/${req.params.name}/`) // get the proper case of the username instead of whatever admin inputted
+            const scratchData = await scratchResponse.json()
 
-            if(!scratchData.username) {
+            if (typeof scratchData.username === "undefined") {
                 return res.json({ error: "user not found on scratch" })
             }
 
-            let now = new Date()
+            const now = new Date()
 
             await users.insert({
                 name: scratchData.username,
@@ -154,31 +159,32 @@ app.put('/api/user/:name', cors(), async (req, res) => {
                     updatedBy: session.name
                 }
             })
-            res.json({ ok: 'user added' })
+            return res.json({ ok: 'user added' })
         }
     }
+    return res.json({ error: 'you need auth' })
 })
 
 app.delete('/api/user/:name', cors(), async (req, res) => {
-    if (!req.headers.authorization) {
+    if (typeof req.headers.authorization !== "string") {
         return res.json({ error: 'you need auth' })
     }
 
-    let session = findSession(req.headers.authorization)
+    const session = findSession(req.headers.authorization)
 
-    if (!session) {
+    if (typeof session === "undefined") {
         return res.json({ error: 'invalid auth' })
     }
 
-    let sessionUser = await getUserData(session.name)
+    const sessionUser = await getUserData(session.name)
 
     if (!sessionUser.admin) {
         return res.json({ error: "this action can only be performed by an admin" })
     }
 
-    let user = await getUserData(req.params.name)
+    const user = await getUserData(req.params.name)
 
-    if (!user) {
+    if (typeof user === "undefined") {
         return res.json({ error: "no user found. cannot delete" })
     }
 
@@ -192,29 +198,28 @@ app.delete('/api/user/:name', cors(), async (req, res) => {
 })
 
 app.get('/api/user/:user/picture', cors(), async (req, res) => {
-    let scratchResponse = await fetch(`https://api.scratch.mit.edu/users/${req.params.user}/`)
-    let scratchData = await scratchResponse.json()
-    let pictureURL = 'https://cdn2.scratch.mit.edu/get_image/user/0_90x90.png'
-    if (scratchData.profile) pictureURL = scratchData.profile.images['90x90']
+    const scratchResponse = await fetch(`https://api.scratch.mit.edu/users/${req.params.user}/`)
+    const scratchData = await scratchResponse.json()
+    const pictureURL = scratchData.profile ? scratchData.profile.images['90x90'] : 'https://cdn2.scratch.mit.edu/get_image/user/0_90x90.png'
     res.redirect(pictureURL)
 })
 
 app.options('/api/starred/:id', cors(corsOptions)) // enable pre-flight request for getting star data
 
 app.get('/api/starred/:id', cors(corsOptions), async (req, res) => { // returns whether the logged in user starred a post
-    if (!req.headers.authorization) {
+    if (typeof req.headers.authorization !== "string") {
         res.json({ error: 'you need auth' })
     } else {
-        let session = findSession(req.headers.authorization)
-        if (!session) {
+        const session = findSession(req.headers.authorization)
+        if (typeof session !== "undefined") {
             return res.json({ error: 'invalid auth' })
         }
-        let user = await getUserData(session.name)
-        if (!user) {
+        const user = await getUserData(session.name)
+        if (typeof user === "undefined") {
             return res.json({ error: 'invalid auth no user found' })
         }
 
-        let starredPost = await stars.findOne({ post: req.params.id, user: user.name })
+        const starredPost = await stars.findOne({ post: req.params.id, user: user.name })
         // console.log({ starredPost, post: req.params.id, user: user.name})
         starredPost ? res.json({ starred: true }) : res.json({ starred: false })
     }
@@ -223,22 +228,22 @@ app.get('/api/starred/:id', cors(corsOptions), async (req, res) => { // returns 
 app.options('/api/star/:id', cors(corsOptions)) // enable pre-flight request starring post
 
 app.post('/api/star/:id', cors(corsOptions), async (req, res) => { // stars a post
-    if (!req.headers.authorization) {
+    if (typeof req.headers.authorization !== "string") {
         res.json({ error: 'you need auth' })
     } else {
-        let session = findSession(req.headers.authorization)
+        const session = findSession(req.headers.authorization)
         if (!session) {
             return res.json({ error: 'invalid auth' })
         }
-        let checkRes = await fetch(`https://scratch.mit.edu/discuss/post/${req.params.id}/source/`) // check if the post really exists
+        const checkRes = await fetch(`https://scratch.mit.edu/discuss/post/${req.params.id}/source/`) // check if the post really exists
         if (!checkRes.ok) {
             return res.json({ error: 'post doesnt exist' })
         }
-        let user = await getUserData(session.name)
-        if (!user) {
+        const user = await getUserData(session.name)
+        if (typeof user === "undefined") {
             return res.json({ error: 'invalid auth no user found' })
         }
-        let starredPost = await stars.findOne({ post: req.params.id, user: user.name })
+        const starredPost = await stars.findOne({ post: req.params.id, user: user.name })
         if (starredPost) {
             // remove star
             await stars.remove(starredPost._id)
@@ -254,15 +259,15 @@ app.post('/api/star/:id', cors(corsOptions), async (req, res) => { // stars a po
 app.options('/api/starred', cors(corsOptions)) // enable pre-flight request for starred post list
 
 app.get('/api/starred', cors(corsOptions), async (req, res) => {  // returns list of starred posts
-    if (!req.headers.authorization) {
+    if (typeof req.headers.authorization !== "string") {
         res.json({ error: 'you need auth' })
     } else {
         let session = findSession(req.headers.authorization)
         if (!session) {
             return res.json({ error: 'invalid auth' })
         }
-        let user = await getUserData(session.name)
-        if (!user) {
+        const user = await getUserData(session.name)
+        if (typeof user === "undefined") {
             return res.json({ error: 'invalid auth no user found' })
         }
         const page = parseInt(req.query.page) || 0;
@@ -290,23 +295,23 @@ app.get('/api/starred', cors(corsOptions), async (req, res) => {  // returns lis
 })
 
 app.get('/api/reactions/:id', cors(), async (req, res) => { // returns all of the reactions for a post
-    let postReactions = await getPostReactions(req.params.id)
+    const postReactions = await getPostReactions(req.params.id)
     res.json(postReactions)
 })
 
 app.options('/api/reactions/:id', cors(corsOptions)) // enable pre-flight request for reacting to a post
 
 app.post('/api/reactions/:id', cors(corsOptions), async (req, res) => { // reacts to a post, then returns new reaction list
-    if (!req.headers.authorization) {
+    if (typeof req.headers.authorization !== "string") {
         res.json({ error: 'you need auth' })
     } else {
-        let session = findSession(req.headers.authorization)
+        const session = findSession(req.headers.authorization)
         if (!session) {
             return res.json({ error: 'invalid auth' })
         }
-        let user = await getUserData(session.name)
-        if (!user) {
-            return res.json({ error: 'invalid auth no user found' })
+        const user = await getUserData(session.name)
+        if (typeof user === "undefined") {
+            return res.json({ error: 'invalid auth; no user found' })
         }
         let checkRes = await fetch(`https://scratch.mit.edu/discuss/post/${req.params.id}/source/`) // check if the post really exists
         if (!checkRes.ok) {
@@ -315,7 +320,7 @@ app.post('/api/reactions/:id', cors(corsOptions), async (req, res) => { // react
         if (!emojis.includes(req.body.emoji)) {
             let reactionWithEmoji = await reactions.findOne({ post: req.params.id, emoji: req.body.emoji }) // find a reaction with that emoji to check if thats a valid reaction option (its set by admin if invalid)
 
-            if (!reactionWithEmoji && !user.admin) return res.json({ error: 'invalid emoji' })
+            if (!reactionWithEmoji && typeof user === "undefined".admin) return res.json({ error: 'invalid emoji' })
         }
         let postReaction = await reactions.findOne({ post: req.params.id, emoji: req.body.emoji, user: user.name })
         if (postReaction) {
@@ -428,14 +433,14 @@ app.post('/auth/remove', cors(corsOptions), async (req, res) => { // used when l
 
 app.options('/auth/me', cors(corsOptions)) // enable pre-flight request for getting user
 app.get('/auth/me', cors(corsOptions), async (req, res) => {
-    if (!req.headers.authorization) {
+    if (typeof req.headers.authorization !== "string") {
         res.json({ error: 'you need auth' })
     } else {
         let session = findSession(req.headers.authorization)
         if (!session) {
             return res.json({ error: 'invalid auth' })
         }
-        let user = await getUserData(session.name)
+        const user = await getUserData(session.name)
         user ? res.json(user) : res.json({ error: "no user found.. this shouldn't happen" })
     }
 })
@@ -447,20 +452,20 @@ app.get('/ban-info/:name', (req, res) => {
 
 // 404. catch all which redirects to frontend
 
-app.use((req, res, next) => {
+app.use((req, res) => {
     res.redirect(`${frontendURL}${req.path}`)
 })
 
 function getUserData(name) {
-    var regexName = "^" + escapeRegExp(name) + "$";
+    const regexName = "^" + escapeRegExp(name) + "$";
     return new Promise(async (resolve, reject) => {
         try {
-            var user = await users.findOne({
+            const user = await users.findOne({
                 name: { $regex: new RegExp(regexName, "i") }
             });
             resolve(user);
         } catch (error) {
-            reject(Error(error));
+            reject(new Error(error));
         }
     })
 }
@@ -478,10 +483,10 @@ async function getPostReactions(id) {
     ]
     */
     return new Promise(async (resolve, reject) => {
-        let postReactions = await reactions.find({ post: id })
+        const postReactions = await reactions.find({ post: id })
         let grouped = []
 
-        let postEmojis = emojis.slice(); // .slice so the original cant be edited
+        let postEmojis = [...emojis];
 
         postReactions.forEach(reaction => {
             if (!postEmojis.includes(reaction.emoji)) postEmojis.push(reaction.emoji)
@@ -500,7 +505,7 @@ function escapeRegExp(string) {
     return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
 }
 
-const groupByKey = (list, key) => list.reduce((hash, obj) => ({ ...hash, [obj[key]]: (hash[obj[key]] || []).concat(obj) }), {})
+// const groupByKey = (list, key) => list.reduce((hash, obj) => ({ ...hash, [obj[key]]: (hash[obj[key]] || []).concat(obj) }), {})
 
 // session management below
 
